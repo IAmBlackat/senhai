656315741a8fdccc8459d361809c04f95ed16829
agustin <agustinagapito09@gmail.com>
2021-02-11 21:49:34 +0800

    darker mode try

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,14 @@
-import { Paper, Typography } from '@material-ui/core'
+import { Box, Paper, Typography } from '@material-ui/core'
 import React from 'react'
 
 function Loading() {
+    // https://i.imgur.com/pMXIhUb.gif
+    // https://danbooru.donmai.us/data/5c61af14520fd187799027fe9207c848.gif
     return(
-        <h1>Loading...</h1>
+        <Box>
+            <h1>Loading...</h1>
+            <img style={{height: '300px', width: 'auto'}} src='https://i.imgur.com/pMXIhUb.gif' alt='' />
+        </Box>
     )
 }
 
